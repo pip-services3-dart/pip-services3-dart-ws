ff92ca65e5de280c0fee42dc640256cb1c81d74d
Sergey Seroukhov <seroukhov@gmail.com>
2020-03-23 08:37:13 -0700

    Initial commit

---
+++ checkout.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+git clone git@github.com:pip-services3-dart/pip-services3-commons-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-components-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-container-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-data-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-mongodb-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-rpc-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-messaging-dart.git
+#git clone git@github.com:pip-services3-dart/pip-services3-grpc-dart.git
+#git clone git@github.com:pip-services3-dart/pip-services3-elasticsearch-dart.git
+#git clone git@github.com:pip-services3-dart/pip-services3-prometheus-dart.git
+#git clone git@github.com:pip-services3-dart/pip-services3-mqtt-dart.git
+#git clone git@github.com:pip-services3-dart/pip-services3-couchbase-dart.git
+#git clone git@github.com:pip-services3-dart/pip-services3-memcached-dart.git
+
+git clone git@github.com:pip-templates/pip-templates-microservice-dart.git
