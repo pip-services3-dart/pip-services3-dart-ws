034335a3bdbcb27667e06f26ff93d6a0a4976e62
Sergey Seroukhov <seroukhov@gmail.com>
2020-04-15 08:37:55 -0700

    Added new components

--- checkout.sh
+++ checkout.sh
@@ -7,11 +7,13 @@ git clone git@github.com:pip-services3-dart/pip-services3-data-dart.git
 git clone git@github.com:pip-services3-dart/pip-services3-mongodb-dart.git
 git clone git@github.com:pip-services3-dart/pip-services3-rpc-dart.git
 git clone git@github.com:pip-services3-dart/pip-services3-messaging-dart.git
-#git clone git@github.com:pip-services3-dart/pip-services3-grpc-dart.git
-#git clone git@github.com:pip-services3-dart/pip-services3-elasticsearch-dart.git
-#git clone git@github.com:pip-services3-dart/pip-services3-prometheus-dart.git
-#git clone git@github.com:pip-services3-dart/pip-services3-mqtt-dart.git
-#git clone git@github.com:pip-services3-dart/pip-services3-couchbase-dart.git
-#git clone git@github.com:pip-services3-dart/pip-services3-memcached-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-grpc-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-elasticsearch-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-prometheus-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-mqtt-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-couchbase-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-memcached-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-redis-dart.git
+git clone git@github.com:pip-services3-dart/pip-services3-rabbitmq-dart.git
 
 git clone git@github.com:pip-templates/pip-templates-microservice-dart.git
